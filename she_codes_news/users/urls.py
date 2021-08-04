02d9df3f80ceb5c187aaf7653a4e00443b78b0e9
runda-git <84775206+runda-git@users.noreply.github.com>
2021-08-04 14:49:03 +0800

    add accounts and profile page

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateAccountViews
+from .views import CreateAccountViews, ProfileView
 
 app_name = 'users'
 
 urlpatterns = [
-    path('create-account/', CreateAccountViews.as_view(), name='createAccount')
+    path('create-account/', CreateAccountViews.as_view(), name='createAccount'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
 ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -9,3 +9,7 @@ class CreateAccountViews(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'user/createAccount.html'
 
+class ProfileView(generic.DetailView):
+    model = CustomUser
+    template_name = 'user/profile.html'
+    context_object_name = 'user'
