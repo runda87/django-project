8722503fce60d0c4438627a44803622ab50fca02
runda-git <84775206+runda-git@users.noreply.github.com>
2021-08-05 17:17:21 +0800

    css updates

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -25,7 +25,7 @@ class IndexView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all().order_by('-pub_date')[:4]
+        context['latest_stories'] = NewsStory.objects.all().order_by('-pub_date')[:3]
         context['all_stories'] = NewsStory.objects.all()
         return context
         
